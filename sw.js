ebf2ee4deef40b1d856ff0b78c604960c9cfb04a
zingYnu <zing@tndstd.com>
2021-11-26 16:17:21 +0800

    pwa应用demo 目前只做了安装，缓存还没弄

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
+
+/** 
+ * 这个模块主要是用来做缓存管理的
+*/
 self.addEventListener('install', function(e) {
     console.log('install success')
   });
-  
-  self.addEventListener('fetch', function(e) {
-    console.log(e.request.url);
-  });
+/** 
+ * 这里拦截网络请求 如果没网就拉取本地缓存
+ * 暂时没做 
+ */
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+});
   
