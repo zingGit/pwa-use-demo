26ae33f92c96db42d8e98752a15c43de37276458
zingYnu <zing@tndstd.com>
2021-11-26 11:47:34 +0800

    jjj

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ if('serviceWorker' in navigator) {
 // window.addEventListener('beforeinstallprompt', (e) => {
   // Chrome 67 及之前版本，会自动展现安装的 prompt
   // 为了版本统一及用户体验，我们禁止自动展现 prompt
-  e.preventDefault();
+//   e.preventDefault();
   // 存放事件用于后续触发
-  deferredPrompt = e;
+//   deferredPrompt = e;
   // 展现按钮
   addBtn.style.display = 'block';
 
   addBtn.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
-    // addBtn.style.display = 'none';
+    addBtn.style.display = 'none';
     // 展现安装的 prompt
     deferredPrompt.prompt();
     // 等待用户对 prompt 进行操作
